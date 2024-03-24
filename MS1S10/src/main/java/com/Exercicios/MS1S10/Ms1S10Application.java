dd09c8c508f550cd739e4ddcaad91a0fcf75a9ce
antonioszm <75432982+antonioszm@users.noreply.github.com>
2024-03-24 19:12:55 -0300

    commit inicial

---
+++ MS1S10/src/main/java/com/Exercicios/MS1S10/Ms1S10Application.java
@@ -0,0 +1,13 @@
+package com.Exercicios.MS1S10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms1S10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms1S10Application.class, args);
+	}
+
+}
---
+++ MS1S10/src/test/java/com/Exercicios/MS1S10/Ms1S10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Exercicios.MS1S10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms1S10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
